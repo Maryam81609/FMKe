261bb1299211ec58cfcd68861272da086fc6cd14
Peter Zeller <p_zeller@cs.uni-kl.de>
2016-12-13 17:15:13 +0100

    use correct endpoints in test

--- src/pharmacy_handler.erl
+++ src/pharmacy_handler.erl
@@ -31,6 +31,8 @@ handle_req(<<"GET">>, true, _Opts, Req) ->
   cowboy_req:reply(400, #{}, ?ERR_BODY_IN_A_GET_REQUEST, Req);
 handle_req(<<"GET">>, false, prescriptions, Req) ->
   get_pharmacy_prescriptions(Req);
+handle_req(<<"GET">>, false, processed_prescriptions, Req) ->
+  get_pharmacy_processed_prescriptions(Req);
 handle_req(<<"GET">>, false, _Opts, Req) ->
   get_pharmacy(Req).
 
@@ -82,6 +84,20 @@ get_pharmacy_prescriptions(Req) ->
       cowboy_req:reply(400, #{}, ?ERR_INVALID_PHARMACY_ID, Req);
     false ->
       ServerResponse = fmk_core:get_pharmacy_prescriptions(IntegerId),
+      JsonReply = jsx:encode([{success, true}, {result, ServerResponse}]),
+      cowboy_req:reply(200, #{
+        <<"content-type">> => <<"application/json">>
+      }, JsonReply, Req)
+  end.
+
+get_pharmacy_processed_prescriptions(Req) ->
+  Id = cowboy_req:binding(?BINDING_PHARMACY_ID, Req, -1),
+  IntegerId = binary_to_integer(Id),
+  case IntegerId =< ?MIN_ID of
+    true ->
+      cowboy_req:reply(400, #{}, ?ERR_INVALID_PHARMACY_ID, Req);
+    false ->
+      ServerResponse = fmk_core:get_processed_pharmacy_prescriptions(IntegerId),
       JsonReply = jsx:encode([{success, true}, {result, ServerResponse}]),
       cowboy_req:reply(200, #{
         <<"content-type">> => <<"application/json">>
